ec80f12f6d85216f83486ef3675fab96499da246
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-05-09 19:42:54 +0600

    JSON Server Setup

--- task-tracker/src/app/components/tasks/tasks.component.ts
+++ task-tracker/src/app/components/tasks/tasks.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TASKS } from 'src/app/mock-tasks';
 import { Task } from 'src/app/Task';
+import { TaskService } from 'src/app/services/task.service';
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent {
-  tasks: Task[] = TASKS;
+  constructor(private taskService:TaskService){}
+  tasks: Task[] = [];
+  ngOnInit():void{
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks=tasks;
+    });
+  }
 }
---
+++ task-tracker/src/app/services/task.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { TASKS } from 'src/app/mock-tasks';
+import { Task } from 'src/app/Task';
+import { Observable, of } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor() { }
+  getTasks():Observable<Task[]>{
+    const tasks = of(TASKS);
+    return tasks;
+  }
+}
