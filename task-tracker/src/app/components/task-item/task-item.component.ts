92ebc133d2f3d0206f20e9e6ef1f8a3151d5fdbb
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-05-09 18:46:24 +0600

    Angular Font Setup

--- task-tracker/src/app/components/task-item/task-item.component.ts
+++ task-tracker/src/app/components/task-item/task-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Task } from 'src/app/Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
@@ -7,4 +8,5 @@ import { Task } from 'src/app/Task';
 })
 export class TaskItemComponent {
 @Input() task: Task;
+faTimes = faTimes;
 }
