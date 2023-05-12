15762f6cebc946e398b9c9637b923cb2e6463a0c
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-05-12 13:00:51 +0600

    Angular Router

--- task-tracker/src/app/app.module.ts
+++ task-tracker/src/app/app.module.ts
@@ -9,6 +9,17 @@ import { TaskItemComponent } from './components/task-item/task-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { FormsModule } from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+const appRoutes: Routes = [
+  {
+    path: '', component: TasksComponent
+  },
+  {
+    path: 'about', component: AboutComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,13 +27,16 @@ import { FormsModule } from '@angular/forms';
     ButtonComponent,
     TasksComponent,
     TaskItemComponent,
-    AddTaskComponent
+    AddTaskComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
--- task-tracker/src/app/components/header/header.component.ts
+++ task-tracker/src/app/components/header/header.component.ts
@@ -7,14 +7,14 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  title = 'Task Tracker';
-  showAddTask: boolean = false;
+  title = 'Task Tracker'; //Title variable is called on the header.component.html
+  showAddTask: boolean = false; //THe Add/Close button is displayed on the basis of the boolean value of showAddTask variable
   subscription: Subscription;
   constructor(private uiService:UiService){
     this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask=value));
   }
   ngOnInit(): void {}
-  toggleAddTask(){
+  toggleAddTask(){ //Executed when onClick() function on button.component.ts is called. It inverts the boolean value of showAddTask
     this.uiService.toggleAddTask();
   }
 }
