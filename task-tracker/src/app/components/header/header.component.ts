52804c0fb533fee1f4accf11a6e63f02382bb421
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-05-12 14:46:59 +0600

    Limit Component to Route

--- task-tracker/src/app/components/header/header.component.ts
+++ task-tracker/src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 import { Subscription } from 'rxjs';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -10,11 +11,14 @@ export class HeaderComponent {
   title = 'Task Tracker'; //Title variable is called on the header.component.html
   showAddTask: boolean = false; //THe Add/Close button is displayed on the basis of the boolean value of showAddTask variable
   subscription: Subscription;
-  constructor(private uiService:UiService){
+  constructor(private uiService:UiService, private router: Router){
     this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask=value));
   }
   ngOnInit(): void {}
   toggleAddTask(){ //Executed when onClick() function on button.component.ts is called. It inverts the boolean value of showAddTask
     this.uiService.toggleAddTask();
+  }
+  hasRouter(route:string){
+    return this.router.url === route;
   }
 }
